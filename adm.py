9c010fb7169463e9258cbdad640d0ccfd5e145a1
joseclaudioals <joseclaudioals109@gmail.com>
2026-05-25 07:06:24 -0300

    adm.py - testando input

--- adm.py
+++ adm.py
@@ -2,16 +2,34 @@ import streamlit as st
 import pandas as pd
 import psycopg2 as pg
 
-
+conn = st.connection("postgre", type="sql")
 tab1, tab2, tab3 = st.tabs(["Editar produtos", "Funções basicas","Logs de administrador"])
 
 with tab1:
     st.header("# Editar produtos")
 with tab2:
     st.header("Funções basicas")
-    if st.button("Ver o valor total de um pedido"):
-        conn = st.connection("postgre", type = "sql")
-        df = conn.query("SELECT * FROM produto", ttl = 660)
+
+    with st.form(key="estoque_critico"):
+        st.markdown("# Verificar estoque critico:")
+        n = st.number_input("quantidade de items a serem visualidada", value=0, step=1)
+
+        bt = st.form_submit_button("buscar")
+
+    if bt:
+        sql = '''
+                SELECT verificar_estoque_critico(:n);
+            '''
+        df = conn.query(sql, params={"n": n}, ttl=660)
+        st.dataframe(df)
+
+    if st.button("Ver todos os produtos"):
+
+
+        sql = '''
+            SELECT * FROM produto;
+        '''
+        df = conn.query(sql,ttl = 660)
         st.dataframe(df)
 with tab3:
     st.header("logs de administrador")
