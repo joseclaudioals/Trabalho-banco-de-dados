d796b8470683d0748d0f43bdf440f9adfd71beb1
joseclaudioals <joseclaudioals109@gmail.com>
2026-05-24 22:39:25 -0300

    adm teste

--- adm.py
+++ adm.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.markdown("# administração")
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+df
 st.sidebar.markdown("# Administrador")
+
